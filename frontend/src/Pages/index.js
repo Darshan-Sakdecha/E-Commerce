accada0130323af4a27cd0057ae9c188f35c3512
darshansakdecha <darshansakdecha@gmail.com>
2025-08-30 15:03:11 +0530

    create a e-commerce project

---
+++ frontend/src/Pages/index.js
@@ -0,0 +1,11 @@
+import About from "./About";
+import Home from "./Home";
+import Collection from "./Collection";
+import Login from "./Login";
+import Orders from "./Orders";
+import PlaceOrder from "./PlaceOrder";
+import Product from "./Product";
+import Contact from "./Contact";
+import Cart from "./Cart";
+
+export { About, Home, Collection, Login, Orders, PlaceOrder, Product, Contact, Cart }
