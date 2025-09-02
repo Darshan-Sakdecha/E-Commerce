1ffe7b17320afdabee24d05a1ea5d603a54b0f33
darshansakdecha <darshansakdecha@gmail.com>
2025-09-02 20:20:33 +0530

    Add SearchBar component and integrate it into App; enhance Navbar for search functionality and update Collection to filter products based on search input

--- frontend/src/Pages/Collection.jsx
+++ frontend/src/Pages/Collection.jsx
@@ -5,11 +5,12 @@ import Title from "../Components/Title";
 import ProductItem from "../Components/ProductItem";
 
 function Collection() {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState("relavent");
 
   const toggleCategory = (e) => {
     if (category.includes(e.target.value)) {
@@ -29,6 +30,13 @@ function Collection() {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
     if (category.length > 0) {
       productsCopy = productsCopy.filter((item) =>
         category.includes(item.category)
@@ -44,13 +52,37 @@ function Collection() {
     setFilterProducts(productsCopy);
   };
 
+  //fpCopy = filter products copy
+  //sort((a, b) => a.price - b.price) → ascending by price
+  //sort((a, b) => b.price - a.price) → descending by price
+
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+    switch (sortType) {
+      case "low-high":
+        setFilterProducts(fpCopy.sort((a, b) => a.price - b.price));
+        break;
+
+      case "high-low":
+        setFilterProducts(fpCopy.sort((a, b) => b.price - a.price));
+        break;
+      default:
+        applyFilter();
+        break;
+    }
+  };
+
   // useEffect(() => {
   //   setFilterProducts(products);
   // }, []);
 
   useEffect(() => {
     applyFilter();
-  }, [category, subCategory]);
+  }, [category, subCategory, search, showSearch]);
+
+  useEffect(() => {
+    sortProduct();
+  }, [sortType]);
 
   // useEffect(() => {
   //   console.log(category);
@@ -155,7 +187,10 @@ function Collection() {
         <div className="flex justify-between text-base sm:text-2xl mb-4">
           <Title text1={"ALL"} text2={"COLLECTIONS"} />
           {/* Product Sort */}
-          <select className="border-2 border-gray-300 text-sm px-2">
+          <select
+            onChange={(e) => setSortType(e.target.value)}
+            className="border-2 border-gray-300 text-sm px-2"
+          >
             <option value="relavent">Sort by: Relavent</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
