95ed9ba16da14c8b194aaf4efb8d676e74c16b7c
darshansakdecha <darshansakdecha@gmail.com>
2025-09-02 22:56:49 +0530

    Refactor SearchBar and Product components for improved readability; implement product data fetching and display logic; update CSS to hide scrollbar

--- frontend/src/Components/SearchBar.jsx
+++ frontend/src/Components/SearchBar.jsx
@@ -6,20 +6,20 @@ import { useLocation } from "react-router-dom";
 function SearchBar() {
   const { search, setSearch, showSearch, setShowSearch } =
     useContext(ShopContext);
-    //useLocation give a current path of your page
-    const location = useLocation();
-    const [visible,setVisible]= useState(false)
+  //useLocation give a current path of your page
+  const location = useLocation();
+  const [visible, setVisible] = useState(false)
 
-    useEffect(()=>{
-        // console.log(location.pathname);
-        if(location.pathname.includes('collection')){
-            setVisible(true)
-        }
-        else{
-            setVisible(false)
-        }
-        
-    },[location])
+  useEffect(() => {
+    // console.log(location.pathname);
+    if (location.pathname.includes('collection')) {
+      setVisible(true)
+    }
+    else {
+      setVisible(false)
+    }
+
+  }, [location])
   return showSearch && visible ? (
     <div className="border-t border-b bg-gray-50 text-center">
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
