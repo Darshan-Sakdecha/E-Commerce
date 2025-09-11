8b1f68b02c27bff8b66ffdeb08dc3d252b541b9d
darshansakdecha <darshansakdecha@gmail.com>
2025-09-11 22:01:09 +0530

    feat: Implement product management features with add, list, and remove functionality; integrate Cloudinary for image uploads; set up multer middleware for file handling

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -38,6 +38,6 @@ const productSchema = new mongoose.Schema({
     }
 })
 
-const ProductModel = mongoose.model("ProductModel,productSchema")
+const ProductModel = mongoose.model("ProductModel",productSchema)
 
 export default ProductModel;
