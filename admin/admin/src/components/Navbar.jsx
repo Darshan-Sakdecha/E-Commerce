3a2239fd489352c96f47e80424ec786779bec500
darshansakdecha <darshansakdecha@gmail.com>
2025-09-15 19:57:47 +0530

    feat: Initialize admin panel with React, Vite, and Tailwind CSS
    - Added package.json and vite.config.js for the admin panel setup
    - Created main application structure with App, Navbar, Sidebar, and Login components
    - Implemented routing for Add, List, and Orders pages
    - Integrated Toast notifications for user feedback
    - Added assets for icons and images
    - Developed backend functionality for product management with admin authentication
    - Enhanced user login with JWT token handling
    - Established middleware for admin authorization

---
+++ admin/admin/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+function Navbar() {
+  return (
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <img src={assets.SHOPfinite} alt="" className='w-40' />
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 rounded-full text-xs sm:text-sm'>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -50,17 +50,36 @@ const addProduct = async (req, res) => {
 
 // function for list product
 const listProducts = async (req, res) => {
-
+    try {
+        const products = await ProductModel.find({});
+        res.json({ success: true, products })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 // function for remove product
 const removeProduct = async (req, res) => {
-
+    try {
+        await ProductModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: "Product Removed" })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 // function for single product info
 const singleProduct = async (req, res) => {
-
+    try {
+        const { productId } = req.body;
+        const product = await ProductModel.findById(productId)
+        res.json({ success: true, product })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 export { addProduct, listProducts, removeProduct, singleProduct }
