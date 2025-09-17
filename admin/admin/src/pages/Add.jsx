08ad9976cc234a6a69d69b3993107473de598190
darshansakdecha <darshansakdecha@gmail.com>
2025-09-17 19:53:14 +0530

    feat: Enhance Add and List components with product management functionality; implement image upload, product listing, and removal features; integrate backend API calls and loading states

--- admin/admin/src/pages/Add.jsx
+++ admin/admin/src/pages/Add.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
 
-const Add = () => {
+
+const Add = ({ token }) => {
 
   const [image1, setImage1] = useState(false)
   const [image2, setImage2] = useState(false)
@@ -15,9 +19,54 @@ const Add = () => {
   const [subCategory, setSubCategory] = useState("Topwear");
   const [bestseller, setBestseller] = useState(false);
   const [sizes, setSizes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const formData = new FormData()
+
+      formData.append("name", name)
+      formData.append("description", description)
+      formData.append("price", price)
+      formData.append("category", category)
+      formData.append("subCategory", subCategory)
+      formData.append("bestSeller", bestseller)
+      formData.append("sizes", JSON.stringify(sizes))
+      image1 && formData.append("image1", image1)
+      image2 && formData.append("image2", image2)
+      image3 && formData.append("image3", image3)
+      image4 && formData.append("image4", image4)
+
+      const response = await axios.post(backendUrl + "/api/product/add", formData, { headers: { token } })
+      // console.log(response.data);
+      if (response.data.success) {
+        toast.success(response.data.message)
+        setName('')
+        setDescription('')
+        setImage1(false)
+        setImage2(false)
+        setImage3(false)
+        setImage4(false)
+        setPrice('')
+      }
+      else {
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
 
   return (
-    <form className='flex flex-col w-full items-start gap-3' action="">
+    <form onSubmit={onSubmitHandler} className='flex flex-col w-full items-start gap-3' action="">
       <div>
         <p className='mb-2'>Upload Image</p>
         <div className='flex gap-2'>
@@ -33,7 +82,7 @@ const Add = () => {
             <img className='w-20' src={!image3 ? assets.upload_area : URL.createObjectURL(image3)} alt="" />
             <input onChange={(e) => setImage3(e.target.files[0])} type="file" id='image3' hidden />
           </label>
-          <label htmlFor="image3">
+          <label htmlFor="image4">
             <img className='w-20' src={!image4 ? assets.upload_area : URL.createObjectURL(image4)} alt="" />
             <input onChange={(e) => setImage4(e.target.files[0])} type="file" id='image4' hidden />
           </label>
@@ -77,29 +126,42 @@ const Add = () => {
         <p className='mb-2'>Product Sizes</p>
         <div className='flex gap-3'>
           <div onClick={() => setSizes(prev => prev.includes("S") ? prev.filter(item => item !== "S") : [...prev, "S"])}>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+            <p className={`${sizes.includes("S") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>S</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          <div onClick={() => setSizes(prev => prev.includes("M") ? prev.filter(item => item !== "M") : [...prev, "M"])}>
+            <p className={`${sizes.includes("M") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>M</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          <div onClick={() => setSizes(prev => prev.includes("L") ? prev.filter(item => item !== "L") : [...prev, "L"])}>
+            <p className={`${sizes.includes("L") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>L</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          <div onClick={() => setSizes(prev => prev.includes("XL") ? prev.filter(item => item !== "XL") : [...prev, "XL"])}>
+            <p className={`${sizes.includes("XL") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>XL</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          <div onClick={() => setSizes(prev => prev.includes("XXL") ? prev.filter(item => item !== "XXL") : [...prev, "XXL"])}>
+            <p className={`${sizes.includes("XXL") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>XXL</p>
           </div>
         </div>
       </div>
 
       <div className='flex gap-2 mt-2 '>
-        <input type="checkbox" id='bestseller' />
+        <input
+          type="checkbox"
+          id="bestseller"
+          checked={bestseller}
+          onChange={(e) => setBestseller(e.target.checked)}
+        />
+
         <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
       </div>
 
-      <button type='submit' className='w-28 py-3 mt-4 bg-black text-white'>ADD</button>
+      <button
+        type="submit"
+        disabled={loading}
+        className="w-28 py-3 mt-4 bg-black text-white flex items-center justify-center"
+      >
+        {loading ? "Adding..." : "ADD"}
+      </button>
+
     </form>
   )
 }
